ff8a04389c125fc94e92345aa698b544f9e7b57e
afrahhh27 <afraibnathossain@gmail.com>
2026-02-11 16:31:42 +0000

    finished implementing most of AirCraft
    just need to figure out how to update altitude

--- airport-app/src/main/java/com/cs261/app/model/AirCraft.java
+++ airport-app/src/main/java/com/cs261/app/model/AirCraft.java
@@ -49,13 +49,14 @@ public class AirCraft {
     private LocalDateTime scheduled; // SCHEDULED arrival/departure time
     private LocalDateTime entryTime; // ACTUAL arrival/departure time i.e. entry time into queue., just the time it enters the simulation, not necessarily the time it leaves the simulation. 
     private LocalDateTime exitTime; // ACTUAL exit time from the simulation.
-    private float altitude;
-    private float fuel;
-    private float speed; // ground speed
+    private double altitude;
+    private double fuel;
+    private double speed; // ground speed
     private EmergencyStatus emergencyStatus;
     private ZoneStatus zoneStatus; // arrived, took off, cancelled, diverted 
     private FlightType flightType; // arrival or departure
-
+    
+    private final double fuelBurn = 2.70;
 
 
     public AirCraft(FlightType type, String callsign, String operator, String origin, String dest, LocalDateTime scheduled, LocalDateTime entryTime, float speed, float fuel, float alt, EmergencyStatus status){
@@ -74,17 +75,146 @@ public class AirCraft {
         this.exitTime = null;
         this.zoneStatus =  ZoneStatus.QUEUE;
     }
-
-// update: fuel, altitude (idk yet for that), emergency according to FUEL
-// setting: exit time, zone status
-// stays constant: callsign, operator, origin, dest, entry time, flighttype, speed
-
-
     /**
+	 * @return the entryTime
+	 */
+	public LocalDateTime getEntryTime() {
+		return entryTime;
+	}
+	/**
+	 * @return the exitTime
+	 */
+	public LocalDateTime getExitTime() {
+		return exitTime;
+	}
+
+	/**
+	 * @param exitTime the exitTime to set
+	 */
+	public void setExitTime(LocalDateTime exitTime) {
+		this.exitTime = exitTime;
+	}
+
+	/**
+	 * @return the zoneStatus
+	 */
+	public ZoneStatus getZoneStatus() {
+		return zoneStatus;
+	}
+
+	/**
+	 * @param zoneStatus the zoneStatus to set
+	 */
+	public void setZoneStatus(ZoneStatus zoneStatus) {
+		this.zoneStatus = zoneStatus;
+	}
+
+	/**
+	 * @return the callSign
+	 */
+	public String getCallSign() {
+		return callSign;
+	}
+
+	/**
+	 * @return the operator
+	 */
+	public String getOperator() {
+		return operator;
+	}
+
+	/**
+	 * @return the origin
+	 */
+	public String getOrigin() {
+		return origin;
+	}
+
+	/**
+	 * @return the destination
+	 */
+	public String getDestination() {
+		return destination;
+	}
+
+	/**
+	 * @return the scheduled
+	 */
+	public LocalDateTime getScheduled() {
+		return scheduled;
+	}
+
+	/**
+	 * @return the altitude
+	 */
+	public float getAltitude() {
+		return altitude;
+	}
+
+	/**
+	 * @return the fuel
+	 */
+	public float getFuel() {
+		return fuel;
+	}
+
+	/**
+	 * @return the speed
+	 */
+	public float getSpeed() {
+		return speed;
+	}
+
+	/**
+	 * @return the emergencyStatus
+	 */
+	public EmergencyStatus getEmergencyStatus() {
+		return emergencyStatus;
+	}
+
+	/**
+	 * @return the flightType
+	 */
+	public FlightType getFlightType() {
+		return flightType;
+	}
+
+	// update: fuel, altitude (idk yet for that), emergency according to FUEL
+	// setting: exit time, zone status
+	// stays constant: callsign, operator, origin, dest, entry time, flighttype, speed
+	
+	/**
      * Decrease fuel by constant amount according to time t
      * @param t fuel decrease in t minutes
      */
     public void updateFuel(int t){
-        this.fuel = this.fuel - (()/0.);;
+        this.fuel = this.fuel - (((this.speed * 1.852) * (t/60) * this.fuelBurn) / 0.8029);
+    }
+    
+    /**
+     * @return the number of hours of fuel remaining 
+     */
+    public double fuelRemainingHrs(){
+    	double hrs = ((this.fuel * 0.8029) / this.fuelBurn) * (1 / this.speed);
+    	return hrs;
+    }
+    
+    /**
+     * TODO: FIX UPDATE METHOD FOR ALTITUDE
+     */
+    public void updateAltitude(){
+    	this.altitude -= 1000;
+    }
+    /**
+     * Updates emergency status if there is a fuel emergency.
+     * @return true if there is a fuel emergency, false otherwise.
+     */
+    public boolean updateFuelEmergency(){
+    	if (fuelRemainingHrs() <= 0.25) {
+    		this.emergencyStatus = EmergencyStatus.FUEL;
+    		return true; // there is a fuel emergency
+    	} else {
+    		return false; // no fuel emergency
+    	}
     }
 }
