3d2233e003459c155f969428fdf98e26032581bb
YunasKramer <yunaskramer@gmail.com>
2026-02-12 21:14:42 +0000

    Create MapsTest.java

---
+++ airport-app/src/test/java/com/cs261/app/model/MapsTest.java
@@ -0,0 +1,25 @@
+package com.cs261.app.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapsTest {
+
+    @Test
+    void aircraftMapstoresAndRetrievesById() {
+        // TODO:
+        // generate AC
+        // insert AC into map
+        // search for the AC
+
+        assertTrue(true);
+    }
+
+    @Test
+    void runwayMapstoresAndRetrievesById() {
+        // TODO:
+        // same as AC but for runway
+
+        assertTrue(true);
+    }
+}
