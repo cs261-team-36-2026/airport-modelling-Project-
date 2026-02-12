e2c84db0bc21525de6a0b49bd02fa2865f6db604
afrahhh27 <afraibnathossain@gmail.com>
2026-02-12 23:32:05 +0000

    implemented aircraftmap, runwaymap, modeloutput

--- airport-app/src/main/java/com/cs261/app/controller/ISimLoop.java
+++ airport-app/src/main/java/com/cs261/app/controller/ISimLoop.java
@@ -1,5 +1,10 @@
 package com.cs261.app.controller;
 
+import com.cs261.app.model.RunWay;
+
 public interface ISimLoop {
     // add testing constructor, testing method, normal constructor and normal method
+	
+	public void testModel(int runtime, RunWay[] runwayList);
+	
 }
--- airport-app/src/main/java/com/cs261/app/model/IModelOutput.java
+++ airport-app/src/main/java/com/cs261/app/model/IModelOutput.java
@@ -2,6 +2,15 @@ package com.cs261.app.model;
 
 public interface IModelOutput {
     /**
-    TODO: add getMaxArrivals etc stuff  
+    doneTODO: add getMaxArrivals etc stuff  
     */ 
+	public void updateOutput();
+	public int maxHoldingSize();
+	public int maxTakeOffSize();
+	public float maxArriveDelay();
+	public float maxDepartDelay();
+	public float avgArriveDelay();
+	public float avgDepartDelay();
+	public float avgDepartQueueTime(); // takeoff queue avg time
+	public float avgArriveQueueTime(); // holding queue avg  time
 }
