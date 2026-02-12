837c0e739d6f8b796ee6056a2c17ec0e33a1e3d2
YunasKramer <yunaskramer@gmail.com>
2026-02-12 21:06:04 +0000

    Create TakeOffQueueTest.java

---
+++ airport-app/src/test/java/com/cs261/app/model/TakeOffQueueTest.java
@@ -0,0 +1,14 @@
+package com.cs261.app.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TakeOffQueueTest {
+
+    @Test
+    void fifo_order_is_preserved() {
+        // TODO: Once TakeOffQueue has enqueue/dequeue methods:
+        assertTrue(true); // placeholder so file compiles
+    }
+
+}
