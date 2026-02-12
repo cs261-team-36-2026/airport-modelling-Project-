f89a753991d167b3e4dfbaadcac12a6a693bbdb4
YunasKramer <yunaskramer@gmail.com>
2026-02-12 21:19:26 +0000

    Update and rename InvariantTest to InvariantTest.java

---
+++ airport-app/src/test/java/com/cs261/app/invariants/InvariantTest.java
@@ -0,0 +1,15 @@
+package com.cs261.app.invariants;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class InvariantTest {
+
+    @Test
+    void aircraftShouldNotExistInMultipleZones() {
+        // TODO: get the state of simulation at a tick
+        // assert aircraft ID is not in holding + takeoff + runway at same time.
+
+        assertTrue(true);
+    }
+}
