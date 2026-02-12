05205cb668346b5dce0386bf941cdd5abddd8c6e
YunasKramer <yunaskramer@gmail.com>
2026-02-12 21:46:14 +0000

    Update IModelOutput.java

--- airport-app/src/main/java/com/cs261/app/model/IModelOutput.java
+++ airport-app/src/main/java/com/cs261/app/model/IModelOutput.java
@@ -4,4 +4,27 @@ public interface IModelOutput {
     /**
     TODO: add getMaxArrivals etc stuff  
     */ 
+    //queue
+    List<String> getHoldingQueueOrder();
+    List<String> getTakeOffQueueOrder();
+    //runway
+    Map<String, String> getRunwayOccupancy();
+    Map<String, String> getRunwayModes();
+    Map<String, Boolean> getRunwayClosureStatus();
+    //aircraft
+    Set<String> getHoldingAircraftIds();
+    Set<String> getTakeOffAircraftIds();
+    Set<String> getRunwayAircraftIds();
+    Set<String> getExitedAircraftIds();
+
+    //metrics
+    int getTotalArrivalsGenerated();
+    int getTotalDeparturesGenerated();
+    int getTotalAircraftLanded();
+    int getTotalAircraftTakenOff();
+    int getMaxHoldingQueueLength();
+    int getMaxTakeOffQueueLength();
+    double getAverageHoldingWaitTime();
+    double getAverageTakeOffWaitTime()
+    
 }
