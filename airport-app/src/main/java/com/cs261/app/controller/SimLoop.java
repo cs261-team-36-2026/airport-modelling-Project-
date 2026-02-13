43863f90c504780161c06a4d96eee805cc1d4b9e
afrahhh27 <afraibnathossain@gmail.com>
2026-02-13 15:57:31 +0000

    added skeleton code to simloop

--- airport-app/src/main/java/com/cs261/app/controller/SimLoop.java
+++ airport-app/src/main/java/com/cs261/app/controller/SimLoop.java
@@ -2,15 +2,51 @@ package com.cs261.app.controller;
 
 import javafx.concurrent.Task;
 import java.util.concurrent.TimeUnit;
-
+import com.cs261.app.model.AirCraftMap;
+import com.cs261.app.model.ArrivalModel;
+import com.cs261.app.model.HoldingQueue;
 import com.cs261.app.model.IModelOutput;
+import com.cs261.app.model.RunWayMap;
+import com.cs261.app.model.TakeOffQueue;
+import com.cs261.app.model.AirCraft;
+import com.cs261.app.model.RunWay;
 
 
 public class SimLoop extends Task<IModelOutput>{
+	private AirCraftMap aircraftMap;
+	private HoldingQueue holdingPattern;
+	private TakeOffQueue takeOffQueue;
+	private RunWayMap runways;
+	private ArrivalModel arrivalModel;
+	private int runtime;
+	private int currentTime;
+	private int prevTime;
+	private final int timeInc = 2;
+	
+	// TODO: add data gen and departure model
+	
+	public SimLoop(RunWay[] userRunways, int userRuntime) {
+		this.runways = new RunWayMap(userRunways);
+		this.runtime = userRuntime;
+		this.currentTime = 0;
+		this.prevTime = 0;
+		this.aircraftMap = new AirCraftMap();
+		this.holdingPattern = new HoldingQueue(500);
+		// take off queue,datagen, departure model
+		this.arrivalModel = new ArrivalModel(aircraftMap, holdingPattern);
+	}
 	
 	@Override
 	protected IModelOutput call() throws Exception {
 		// TODO Auto-generated method stub
+		while (currentTime != runtime) {
+			// traffic controller update
+			// arrivals update
+			arrivalModel.updateArrivals(null, currentTime, prevTime);
+			// departures update
+			currentTime += timeInc;
+		}
+		
 		return null;
 	}
 
