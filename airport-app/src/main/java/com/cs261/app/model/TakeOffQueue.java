d93eba78bcc2aab9643f7d12740c2b5b433c9b3a
1-PJs-1 <68129967+1-PJs-1@users.noreply.github.com>
2026-02-19 14:55:51 +0000

    takeoff queue, needs testing, need to add some metrics

--- airport-app/src/main/java/com/cs261/app/model/TakeOffQueue.java
+++ airport-app/src/main/java/com/cs261/app/model/TakeOffQueue.java
@@ -1,7 +1,46 @@
 package com.cs261.app.model;
+import java.util.LinkedList;
 
 public class TakeOffQueue {
-    /**
-     * TODO: extends conurrent linked list. doesnt need any custom methods for now. 
+    /** 
+     * is there anything else we want the queue to do? TODO max takeoff queue length
+     * test, calculate metrics here?
      */
+
+    private LinkedList<AirCraft> queue;
+
+    public TakeOffQueue() {
+        this.queue = new LinkedList<>();
+    }
+    // adds aircraft to the back of the queue
+    public void enqueue(AirCraft aircraft) {
+        queue.addLast(aircraft);
+    }
+
+    // removes aircraft from front of queue
+    public AirCraft dequeue() {
+        if (queue.isEmpty()) {
+            return null;  // TODO might want to change to throw exception
+        }
+        return queue.removeFirst();
+    }
+
+    // returns the aircraft at a specific position in the queue
+    public AirCraft getAircraftAt(int index) {
+        if (index < 0 || index >= queue.size()) {
+            return null; // TODO might want to change to throw exception
+        }
+        return queue.get(index);
+    }
+    // checks if queue is empty
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    // returns the size of the queue
+    public int size() {
+        return queue.size();
+    }
+
 }
+
