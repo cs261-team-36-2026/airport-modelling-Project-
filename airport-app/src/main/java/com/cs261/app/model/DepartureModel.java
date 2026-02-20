6c3cfdf3ebeefa6443b06a3560e20e30d963c14c
1-PJs-1 <68129967+1-PJs-1@users.noreply.github.com>
2026-02-20 03:12:29 +0000

    add + remove from takeoff queue, need to add where to store time enters queue

--- airport-app/src/main/java/com/cs261/app/model/DepartureModel.java
+++ airport-app/src/main/java/com/cs261/app/model/DepartureModel.java
@@ -1,6 +1,6 @@
 package com.cs261.app.model;
 
-// TODO need to remove planes so only the current ones in the queue are in here?
+// TODO need to add where time enter queue stored
 
 public class DepartureModel {
 	private AirCraftMap aircraftMap;
@@ -12,8 +12,16 @@ public class DepartureModel {
 		this.takeoffQueue = queue;
 	}
 	
-	public void updateDepartures(AirCraft newPlane, int currentTime, int prevTime) {
+	// public void updateDepartures(AirCraft newPlane, int currentTime, int prevTime) {
+	// 	takeoffQueue.enqueue(newPlane);
+	// }
+ 
+	public void clearedForDeparture(AirCraft newPlane, int currentTime, int prevTime) {
 		takeoffQueue.enqueue(newPlane);
+	}
+
+	public void planeTakesOff(AirCraft newPlane, int currentTime, int prevTime) {
+		takeoffQueue.dequeue();
 	}
 
 }
